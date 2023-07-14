bc563987dbc78e02d2e77af2b55cd1d1f07d8950
Shivam-tomar9 <130753871+Shivam-tomar9@users.noreply.github.com>
2023-07-14 16:43:23 +0530

    Add files via upload

--- AuthController.php
+++ AuthController.php
@@ -1,60 +1,66 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Register;
-use App\Models\User;
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function view()
+    public function loginView()
     {
-    return view('auth/register');
+        return view('auth.login');
     }
+        public function submitLogin(Request $request)
+        {
+           
+            $credentials = $request->validate([
+                'email'=>'required|email',
+                'password'=>'required'
+            ]);
     
-    public function dash()
+        if(Auth::guard('admin')->attempt($credentials)){
+            return redirect()->route('content');
+        }else{
+            return redirect()->back()->with('error','Invalid.');
+        }
+        }
+    
+    public function registerationView()
     {
-        return view('admin_layout.dash');
+        return view('auth.registeration');
     }
-    public function save(Request $request)
+    public function store(Request $request)
     {
-
+        
         $request->validate([
-            'email'=>'required|email|max:255|unique:users,email',
+            'email'=>'required|email|unique:admin,email',
             'password'=>'required',
             'name'=>'required'
 
         ]);
-        $input = $request->all();
-        $input['password'] = Hash::make($request->password);
-        unset($input['_token']);
-        User::insert($input);
-        return redirect("/login");
+        $admin = new Admin();
+        $admin->name = $request->input('name');
+        $admin->email = $request->input('email');
+        $admin->password = Hash::make($request->input('password'));
+        $admin->save();
 
+        return back();
     }
-    public function login()
+    
+    public function resetView()
     {
-        return view('auth.login');
+        return view('auth.reset');
     }
-
-    public function submitLogin(Request $request)
+    public function updateView()
     {
-        $credentials = $request->validate([
-            'email'=>'required|email',
-            'password'=>'required'
-        ]);
-
-    if(Auth::attempt($credentials)){
-        return redirect()->route('dash');
-    }else{
-        return redirect()->back()->with('error','Invalid.');
+        return view('auth.update');
+        
     }
-    }
-
-    public function logout(){
-        Auth::logout();
-        return redirect()->route('login');
+    public function statusView()
+    {
+        return view('auth.status');
     }
 }
+    
